da4c9cec63e85cd21558300fe665254387de1a8c
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-07 20:23:22 -0800

    updated files

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE users (
+CREATE TABLE Burgers (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
     isEaten BOOL NOT NULL
