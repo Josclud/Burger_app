e0b6472bf7b86a8ad59bf59e639155f2740e289b
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-07 18:45:32 -0800

    updated code

---
+++ sql/seed.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (name, isEaten)
+VALUES ('Mcdonalds', true), 
+('In-in-Out', false), 
+('Jack in the box', true), 
+('The Habit', false), 
+('Jenny Doe', false);
