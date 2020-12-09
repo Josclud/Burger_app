012b05309c31a4e08c799befe62215dd057b6406
Joseph Wong-Ludwiczak <josclud@gmail.com>
2020-12-08 17:23:30 -0800

    changes to connection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:Monkey23@localhost/burgers_db')
+const connection = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:Monkey23@localhost/wbgtq288ibu8axko')
 
 module.exports = connection
